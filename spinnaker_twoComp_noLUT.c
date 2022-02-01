c27c11dad34321ff06bffd3ecfa011ef24e84e10
mollie-ward <54624259+mollie-ward@users.noreply.github.com>
2022-02-01 14:05:01 +0000

    Update spinnaker_twoComp_noLUT.c

--- spinnaker_twoComp_noLUT.c
+++ spinnaker_twoComp_noLUT.c
@@ -1,94 +1,85 @@
-#include <qpe.h>  // required qpe support functions
-#include <nmu.h>  // neuromorphic accelerators
-
-int main ()
-{
-  static uint32_t num_data;
-  static uint32_t num_data;
-
   // define constants and variables
-  static float dt = 0.1;
-  static float V_dend;
+  s1615 dt = 0.1;
+  s1615 V_dend;
   V_dend = -65;
-  static float Ie_dend = 35.80986219567645;
-  static float dCaAP_count = 0;
-  static float t_dCaAP;
+  s1615 Ie_dend = 35.80986219567645;
+  s1615 dCaAP_count = 0;
+  s1615 t_dCaAP;
   t_dCaAP = -200;
-  static float K = 0;
-  static float i_dCaAP = 0;
-  static float v_th = -36;
-  static float refract_period = 200;
-  static float AB_refract_period = 77.7;
-  static float denom = -0.08547009;
-  static float w = 0.2;
-  static float this_t = -0.05;
-  static uint32_t count = 0;
-  static float AB = 0;
-  static float totGE_dend = -70;
-  static float C_dend = -1;
-  static float F_dend;
-  static float b_dend = 0;
-  static float c_dend = -0.1;
-  static float f_dend;
-  static float c_x_dend;
-  static float f_x_dend;
-  static float dV_dend;
-  static float this_Ie_dend;
+  s1615 K = 0;
+  s1615 i_dCaAP = 0;
+  s1615 v_th = -36;
+  s1615 refract_period = 200;
+  s1615 AB_refract_period = 77.7;
+  s1615 denom = -0.08547009;
+  s1615 w = 0.2;
+  s1615 this_t = -0.05;
+  int count = 0;
+  s1615 AB = 0;
+  s1615 totGE_dend = -70;
+  s1615 C_dend = -1;
+  s1615 F_dend;
+  s1615 b_dend = 0;
+  s1615 c_dend = -0.1;
+  s1615 f_dend;
+  s1615 c_x_dend;
+  s1615 f_x_dend;
+  s1615 dV_dend;
+  s1615 this_Ie_dend;
 
-  static float V_soma;
-  static float v_rest = -100;
+  s1615 V_soma;
+  s1615 v_rest = -100;
   V_soma = -65;
-  static float m = 0;
-  static float n = 0;
-  static float h = 0;
-  static float totG_soma;
-  static float totGE_soma;
-  static float gNa = 0.12;
-  static float gK = 0.036;
-  static float gL = 0.0003;
-  static float El = -54.3;
-  static float ENa = 50;
-  static float Ek = -77;
-  static float alpha_m;
-  static float beta_m;
-  static float alpha_h;
-  static float beta_h;
-  static float alpha_n;
-  static float beta_n;
-  static float tau_m;
-  static float tau_h;
-  static float tau_n;
-  static float m_inf;
-  static float h_inf;
-  static float n_inf;
-  static float Ie_soma = 7.957747154594768;
-  static float g_soma = 0.1;
-  static float C_soma;
-  static float D_soma = 0.1;
-  static float F_soma;
-  static float c_soma;
-  static float d_soma = 0.01;
-  static float f_soma;
-  static float c_x_soma;
-  static float f_x_soma;
-  static float dV_soma;
+  s1615 m = 0;
+  s1615 n = 0;
+  s1615 h = 0;
+  s1615 totG_soma;
+  s1615 totGE_soma;
+  s1615 gNa = 0.12;
+  s1615 gK = 0.036;
+  s1615 gL = 0.0003;
+  s1615 El = -54.3;
+  s1615 ENa = 50;
+  s1615 Ek = -77;
+  s1615 alpha_m;
+  s1615 beta_m;
+  s1615 alpha_h;
+  s1615 beta_h;
+  s1615 alpha_n;
+  s1615 beta_n;
+  s1615 tau_m;
+  s1615 tau_h;
+  s1615 tau_n;
+  s1615 m_inf;
+  s1615 h_inf;
+  s1615 n_inf;
+  s1615 Ie_soma = 7.957747154594768;
+  s1615 g_soma = 0.1;
+  s1615 C_soma;
+  s1615 D_soma = 0.1;
+  s1615 F_soma;
+  s1615 c_soma;
+  s1615 d_soma = 0.01;
+  s1615 f_soma;
+  s1615 c_x_soma;
+  s1615 f_x_soma;
+  s1615 dV_soma;
 
 
-  
-  alpha_m = (0.1*(V_soma+40)) / (1 - hwexpf(-0.1*(V_soma+40)));
-  beta_m =  4*hwexpf(-0.0556*(V_soma+65));
+  alpha_m = (0.1*(V_soma+40)) / (1 - expk(-0.1*(V_soma+40)));
+  beta_m =  4*expk(-0.0556*(V_soma+65));
   tau_m = 1/(alpha_m+beta_m);
   m_inf = alpha_m/(alpha_m+beta_m);
   m = m_inf;
 
-  alpha_h = 0.07*hwexpf(-0.05*(V_soma+65));
-  beta_h =  1 / (1+hwexpf(-0.1*(V_soma+35)));
+  alpha_h = 0.07*expk(-0.05*(V_soma+65));
+  beta_h =  1 / (1+expk(-0.1*(V_soma+35)));
   tau_h = 1/(alpha_h+beta_h);
   h_inf = alpha_h/(alpha_h+beta_h);
   h = h_inf;
 
-  alpha_n = (0.01*(V_soma+55)) / (1 - hwexpf(-0.1*(V_soma+55)));
-  beta_n = 0.125*hwexpf(-0.0125*(V_soma+65));
+  alpha_n = (0.01*(V_soma+55)) / (1 - expk(-0.1*(V_soma+55)));
+  beta_n = 0.125*expk(-0.0125*(V_soma+65));
   tau_n = 1/(alpha_n+beta_n);
   n_inf = alpha_n/(alpha_n+beta_n);
   n = n_inf;
@@ -96,7 +87,7 @@ int main ()
   
   num_data = 10000;
   
-  for (uint32_t i = 0; i < num_data; i++) {
+  for (int i = 0; i < num_data; i++) {
 
       // use values of m, n and h to calculate current and update membrane voltage
       totG_soma =  (gL + gK * (n*n*n*n) + gNa * (m*m*m) * h)*1000;
@@ -106,10 +97,10 @@ int main ()
       this_t = (i*dt) - (dt/2);
 
 
-		  A_dCaAP = A_dCaAP + (1. - hwexpf(dt * (((((1.0 - A_dCaAP)) + (A_dCaAP) * (((- 1.0))))) / tau_A))) * (- A_dCaAP);
-// 	   B_dCaAP = B_dCaAP * (hwexpf(x2 - y2*B_dCaAP));
+		  A_dCaAP = A_dCaAP + (1. - expk(dt * (((((1.0 - A_dCaAP)) + (A_dCaAP) * (((- 1.0))))) / tau_A))) * (- A_dCaAP);
+// 	   B_dCaAP = B_dCaAP * (expk(x2 - y2*B_dCaAP));
 
-		  B_dCaAP = B_dCaAP + (1. - hwexpf(dt * ((((1.0) * ((1.0 - B_dCaAP)) + (B_dCaAP) * (((- 1.0))))) / tau_B))) * (- B_dCaAP);
+		  B_dCaAP = B_dCaAP + (1. - expk(dt * ((((1.0) * ((1.0 - B_dCaAP)) + (B_dCaAP) * (((- 1.0))))) / tau_B))) * (- B_dCaAP);
 
 
 		  if (B_dCaAP == 0 && this_t > t_dCaAP + sigma_diff)	{
@@ -120,7 +111,7 @@ int main ()
 			  t_dCaAP = this_t;
 			  A_dCaAP = 0.001k;
 			  B_dCaAP = 0;
-        K = hwexpf((V_dend - v_th) * denom);
+        K = expk((V_dend - v_th) * denom);
 
         if (K > 1) {
           K = 1;
@@ -156,24 +147,24 @@ int main ()
 
       // update m, n and h using updated membrane voltage 
       // update m, n and h using updated membrane voltage 
-      alpha_m = (0.1*(V_soma+40)) / (1 - hwexpf(-0.1*(V_soma+40)));
-      beta_m =  4*hwexpf(-0.0556*(V_soma+65));
+      alpha_m = (0.1*(V_soma+40)) / (1 - expk(-0.1*(V_soma+40)));
+      beta_m =  4*expk(-0.0556*(V_soma+65));
       tau_m = 1/(alpha_m+beta_m);
       m_inf = alpha_m/(alpha_m+beta_m);
-      m = m_inf + (m-m_inf)*hwexpf(-dt/tau_m);
+      m = m_inf + (m-m_inf)*expk(-dt/tau_m);
 
 
-      alpha_h = 0.07*hwexpf(-0.05*(V_soma+65));
-      beta_h =  1 / (1+hwexpf(-0.1*(V_soma+35)));
+      alpha_h = 0.07*expk(-0.05*(V_soma+65));
+      beta_h =  1 / (1+expk(-0.1*(V_soma+35)));
       tau_h = 1/(alpha_h+beta_h);
       h_inf = alpha_h/(alpha_h+beta_h);
-      h = h_inf + (h-h_inf)*hwexpf(-dt/tau_h);
+      h = h_inf + (h-h_inf)*expk(-dt/tau_h);
 
-      alpha_n = (0.01*(V_soma+55)) / (1 - hwexpf(-0.1*(V_soma+55)));
-      beta_n = 0.125*hwexpf(-0.0125*(V_soma+65));
+      alpha_n = (0.01*(V_soma+55)) / (1 - expk(-0.1*(V_soma+55)));
+      beta_n = 0.125*expk(-0.0125*(V_soma+65));
       tau_n = 1/(alpha_n+beta_n);
       n_inf = alpha_n/(alpha_n+beta_n);
-      n = n_inf + (n-n_inf)*hwexpf(-dt/tau_n);
+      n = n_inf + (n-n_inf)*expk(-dt/tau_n);
     
   }
 }
